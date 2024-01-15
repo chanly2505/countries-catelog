05b9ca094e35d02d9128cdf445bf93eac4002e25
Ly Chan <dev2@poscardigital.com>
2024-01-15 16:20:34 +0700

    Last Update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
       <BrowserRouter>
           <Routes>
-              <Route path='/' element={<HomePage/>}></Route>
+              <Route path='/countries-catelog' element={<HomePage/>}></Route>
               <Route path='*' element={<Error/>}></Route>
           </Routes>
       </BrowserRouter>
